3ca0256300d8f2e700b5fe445812cdaced91e5b5
Gyeong Dok Ko <42568666+lemidia@users.noreply.github.com>
2020-01-09 08:05:28 +0900

    weather-app added

---
+++ weather-app/app.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', () =>{
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition( position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/0b4a39b98b9129fdc454d3116ee13ad9/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                 return response.json();
+                })
+                 .then(data => {
+                   console.log(data);
+                   const {dewPoint, summary, icon} = data.currently;
+                   // Set DOM Elements from the API
+                   temperatureDegree.textContent = Math.round((dewPoint - 32) / 1.8);
+                   temperatureDescription.textContent = summary;
+                   locationTimezone.textContent = data.timezone.replace("/", " / ");
+                   setIcons(icon, "icon1");
+                  });
+
+        }, () => {
+            alert("Error");
+        });
+    } else{
+        alert("Geolocation is not supported by your browser");
+    }
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color : "white"});
+        const weatherCondition = icon.replace(/-/g, "_").toUpperCase();
+        skycons.set(iconID, Skycons[weatherCondition]);
+        skycons.play();
+    }
+
+    let degreeSection = document.querySelector('.degree-section');
+    let transition = document.querySelector('.transition');
+    let isCelsius = true;
+    degreeSection.addEventListener('click', () => {
+        if (isCelsius) {
+            temperatureDegree.textContent = Math.round(parseInt(temperatureDegree.textContent) * 1.8 + 32);
+            console.log(temperatureDegree);
+            transition.textContent = 'F';
+            isCelsius = false;
+        }else{
+            temperatureDegree.textContent = Math.round((parseInt(temperatureDegree.textContent) - 32) / 1.8);
+            console.log(temperatureDegree);
+            transition.textContent = 'C';
+            isCelsius = true;
+        }
+    })
+});
