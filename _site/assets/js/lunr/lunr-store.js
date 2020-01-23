96842865cc5dde4795cc0dbbd586ad5439d90169
Gyeong Dok Ko <42568666+lemidia@users.noreply.github.com>
2020-01-23 21:16:43 +0900

    files added

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "Union Find",
+        "excerpt":"Union by Rank 트리의 깊이 또는 높이는 유니온 파인드 연산 실행시간에 영향을 주는데 트리를 합칠 때 높이가 작은 트리를 높이가 큰 트리의 루트에 붙이면 높이가 높아지지 않습니다. 이렇게 유니온 바이 랭크(union by rank)는 높이가 작은 트리를 큰 트리 루트에 붙이는 방법입니다. ※ 단, 높이가 같은 트리를 합칠 땐 높이가 +1...","categories": ["윤동주"],
+        "tags": ["서시","윤동주","시","Data Structure"],
+        "url": "http://localhost:4000/%EC%9C%A4%EB%8F%99%EC%A3%BC/si-susi/",
+        "teaser":"http://localhost:4000/assets/images/logo.png"},{
         "title": "github.io 블로그 시작하기",
         "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2019-04-13...","categories": ["Blog"],
         "tags": ["Blog"],
