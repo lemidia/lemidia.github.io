370279cdc622d8873373992391adcb08734c365d
Gyeong Dok Ko <42568666+lemidia@users.noreply.github.com>
2020-02-28 18:31:08 +0900

    fine-dust-app modified

--- fine-dust-app/index.js
+++ fine-dust-app/index.js
@@ -129,7 +129,7 @@ window.addEventListener('load', () =>{
         let currentTime = document.querySelector('.time')
         let currentCondition = document.querySelector('.condition')
         currentLocation.textContent = location
-        currentTime.textContent = 'Updated Time : ' + convert(dateTime)
+        currentTime.textContent = '업데이트 시간 : ' + convert(dateTime)
         currentCondition.textContent = main
 
         // .temp-information
@@ -138,11 +138,11 @@ window.addEventListener('load', () =>{
         let currentCloud = document.querySelector('.cloudiness')
         let currentHumidity = document.querySelector('.humidity')
         let currentWind = document.querySelector('.wind')
-        currentImgIcon.setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+        currentImgIcon.setAttribute('src', `./img/${icon}.png`)
         currentTemp.textContent = Math.round(temp - 273.15)  + '°C' // Kelvin to Celsius
-        currentCloud.textContent = 'Cloudiness : ' + all + '%'
-        currentHumidity.textContent = 'Humidity : ' + humidity + '%'
-        currentWind.textContent = 'Wind : ' + speed + ' m/s'
+        currentCloud.textContent = '구름 : ' + all + '%'
+        currentHumidity.textContent = '습도 : ' + humidity + '%'
+        currentWind.textContent = '바람 : ' + speed + ' m/s'
 
     }
 
@@ -156,56 +156,75 @@ window.addEventListener('load', () =>{
             '-':{
                 condition: '데이터 없음',
                 description: '현재 측정소에서 수집된 데이터가 없습니다 ',
+                color: 'rgb(51, 133, 255)',
                 background: 'linear-gradient(rgb(51, 133, 255), rgb(45, 97, 175))',
-                emoji: 'fas fa-times'
+                emoji: 'fas fa-times',
+                circle: 'light-blue'
             },
             1:{
                 condition: '아주 좋음',
                 description: '현재 공기가 매우 좋습니다 ^o^! ',
+                color: 'rgb(45, 97, 175)',
                 background: 'linear-gradient(rgb(51, 133, 255), rgb(45, 97, 175))',
-                emoji: 'far fa-laugh-beam'
+                emoji: 'far fa-laugh-beam',
+                circle: 'light-blue'
             },
             2:{
                 condition: '좋음',
                 description: '현재 공기가 좋습니다 ^o^ ',
+                color: 'rgb(79,153, 202)',
                 background: 'linear-gradient(rgb(79, 163, 202), rgb(31, 115, 163))',
-                emoji: 'far fa-grin-alt'
+                emoji: 'far fa-grin-alt',
+                circle: 'sky-blue'
             },
             3:{
                 condition: '양호',
                 description: '현재 공기가 양호합니다.',
+                color: 'rgb(46, 143, 156)',
                 background: 'linear-gradient(rgb(46, 143, 156), rgb(48, 62, 143))',
-                emoji: 'far fa-grin-wink'
+                emoji: 'far fa-grin-wink',
+                circle: 'cobalt'
             },
             4:{
                 condition: '보통',
                 description: '밖에 나갈 때 마스크를 착용해주세요.',
+                color: 'rgb(62, 182, 140)',
                 background: 'linear-gradient(rgb(62, 182, 126), rgb(33, 107, 62))',
-                emoji: 'far fa-meh'
+                emoji: 'far fa-meh',
+                circle: 'green'
             },
             5:{
                 condition: '나쁨',
                 description: '되도록 외출을 자제해 주세요.',
+                color: 'rgb(255, 179, 102)',
                 background: 'linear-gradient(rgb(255, 179, 102), rgb(230, 115, 0))',
-                emoji: 'far fa-frown'
+                emoji: 'far fa-frown',
+                circle: 'light blue',
+                circle: 'orange'
             },
             6:{
                 condition: '상당히 나쁨',
                 description: '되도록 외출을 자제해 주세요.',
+                color: 'rgb(230, 92, 0)',
                 background: 'linear-gradient(rgb(230, 115, 0), rgb(230, 92, 0))',
-                emoji: 'far fa-angry'
+                emoji: 'far fa-angry',
+                circle: 'orange'
             },
             7:{
                 condition: '매우 나쁨',
                 description: '외출하지 마시길 권유드립니다.',
+                color: 'rgb(255, 51, 0)',
                 background: 'linear-gradient(rgb(255, 92, 51), rgb(255, 51, 0))',
-                emoji: 'far fa-tired'
+                emoji: 'far fa-tired',
+                circle: 'red'
             },
             8:{
                 condition: '최악',
                 description: '미세먼지 최악입니다. 나가지 마세요.',
+                color: 'rgb(31, 46, 46)',
                 background: 'linear-gradient(rgb(58, 61, 66), rgb(31, 46, 46))',
-                emoji: 'far fa-dizzy'
+                emoji: 'far fa-dizzy',
+                circle: 'black'
             }
         }
 
@@ -253,6 +272,72 @@ window.addEventListener('load', () =>{
                 return 8
             }
         }
+
+        function getO3Grade(value){
+            if (value === '-'){
+                return '-'
+            }else if (value <= 0.02) {
+                return 1
+            }else if (value <= 0.03) {
+                return 2
+            }else if (value <= 0.06) {
+                return 3
+            }else if (value <= 0.09) {
+                return 4
+            }else if (value <= 0.12) {
+                return 5
+            }else if (value <= 0.15) {
+                return 6
+            }else if (value <= 0.38) {
+                return 7
+            }else {
+                return 8
+            }
+        }
+
+        function getCoGrade(value){
+            if (value === '-'){
+                return '-'
+            }else if (value <= 1) {
+                return 1
+            }else if (value <= 2) {
+                return 2
+            }else if (value <= 5.5) {
+                return 3
+            }else if (value <= 9) {
+                return 4
+            }else if (value <= 12) {
+                return 5
+            }else if (value <= 15) {
+                return 6
+            }else if (value <= 32) {
+                return 7
+            }else {
+                return 8
+            }
+        }
+
+        function getNo2Grade(value){
+            if (value === '-'){
+                return '-'
+            }else if (value <= 0.02) {
+                return 1
+            }else if (value <= 0.03) {
+                return 2
+            }else if (value <= 0.05) {
+                return 3
+            }else if (value <= 0.06) {
+                return 4
+            }else if (value <= 0.13) {
+                return 5
+            }else if (value <= 0.2) {
+                return 6
+            }else if (value <= 1.1) {
+                return 7
+            }else {
+                return 8
+            }
+        }
         
         // highestGrade among all values, base value is 1
         let highestGrade = -1
@@ -265,16 +350,27 @@ window.addEventListener('load', () =>{
             highestGrade = currentGrade > highestGrade ? currentGrade : highestGrade
         }
     
-        let pm10ValueElement = document.querySelector('.pm10-value')
         let pm10ConditionElement = document.getElementById('pm10-condition')
 
-        // Set value
-        pm10ValueElement.textContent = pm10Value
+        // Set value & color
         pm10ConditionElement.textContent = WTO_Standard[currentGrade].condition
+        pm10ConditionElement.style.color = WTO_Standard[currentGrade].color
 
-        // Set emoji
+        // Set emoji & its color
         let icon = document.getElementById('pm10-icon')
         icon.className = WTO_Standard[currentGrade].emoji
+        icon.style.color = WTO_Standard[currentGrade].color
+
+        let pm10Circle = document.querySelector('.pm10-circle')
+        if (pm10Value !== '-') {
+            let pm10Percent = Math.round((pm10Value/151) * 100)
+            pm10Circle.className = `pm10-circle c100 p${pm10Percent} small`
+        }else{
+            pm10Circle.className = `pm10-circle c100 p${0} small`
+        }
+        let = pm10CircleValue = document.querySelector('.pm10-circle-Value')
+        pm10CircleValue.textContent = pm10Value + '㎍/㎥'
+        pm10CircleValue.style.color = WTO_Standard[currentGrade].color
 
         // pm 2.5
         currentGrade = getPm25Grade(pm25Value)
@@ -282,14 +378,25 @@ window.addEventListener('load', () =>{
             highestGrade = currentGrade > highestGrade ? currentGrade : highestGrade
         }
     
-        let pm25ValueElement = document.querySelector('.pm25-value')
         let pm25ConditionElement = document.getElementById('pm25-condition')
 
-        pm25ValueElement.textContent = pm25Value
         pm25ConditionElement.textContent = WTO_Standard[currentGrade].condition
+        pm25ConditionElement.style.color = WTO_Standard[currentGrade].color
 
         icon = document.getElementById('pm25-icon')
         icon.className = WTO_Standard[currentGrade].emoji
+        icon.style.color = WTO_Standard[currentGrade].color
+
+        let pm25Circle = document.querySelector('.pm25-circle')
+        if (pm25Value !== '-') {
+            let pm25Percent = Math.round((pm25Value/71) * 100)
+            pm25Circle.className = `pm25-circle c100 p${pm25Percent} small`
+        }else{
+            pm25Circle.className = `pm25-circle c100 p${0} small`
+        }
+        let = pm25CircleValue = document.querySelector('.pm25-circle-Value')
+        pm25CircleValue.textContent = pm25Value + '㎍/㎥'
+        pm25CircleValue.style.color = WTO_Standard[currentGrade].color
 
         // Set background color based on highestGrade 
         if (highestGrade === -1) {
@@ -297,17 +404,69 @@ window.addEventListener('load', () =>{
         }
 
         let background = document.querySelector('.container')
-        background.style.background = WTO_Standard[highestGrade].background
+        //background.style.background = WTO_Standard[highestGrade].background
         
         // Set worst case pm based on highestGrade
         let pmConditionElement = document.getElementById('pm-condition')
         let pmDescriptionElement = document.getElementById('pm-description')
 
-        pmConditionElement.textContent = WTO_Standard[highestGrade].condition
-        pmDescriptionElement.textContent = WTO_Standard[highestGrade].description
+        //pmConditionElement.textContent = WTO_Standard[highestGrade].condition
+        //pmDescriptionElement.textContent = WTO_Standard[highestGrade].description
 
         icon = document.getElementById('pm-icon')
-        icon.className = WTO_Standard[highestGrade].emoji
+        //icon.className = WTO_Standard[highestGrade].emoji
+
+        // etc container
+
+        currentGrade = getO3Grade(o3Value)
+        document.querySelector('.o3-value').textContent = o3Value + 'ppm'
+        const o3IconElement = document.getElementById('03-icon')
+        o3IconElement.className = WTO_Standard[currentGrade].emoji
+        o3IconElement.style.color = WTO_Standard[currentGrade].color
+        const o3ConditionElement = document.querySelector('.o3-condition')
+        o3ConditionElement.textContent = WTO_Standard[currentGrade].condition
+        o3ConditionElement.style.color = WTO_Standard[currentGrade].color
+
+        let o3Percent = Math.round((o3Value/0.09)*100)
+        const o3Progress = document.querySelector('.o3-progress-done');
+        o3Progress.setAttribute('data-done', o3Percent)
+        o3Progress.style.width = o3Progress.getAttribute('data-done') + '%';
+        o3Progress.style.background = WTO_Standard[currentGrade].background
+        o3Progress.style.opacity = 1;
+
+        currentGrade = getCoGrade(coValue)
+        document.querySelector('.co-value').textContent = coValue + 'ppm'
+        const coIconElement = document.getElementById('co-icon')
+        coIconElement.className = WTO_Standard[currentGrade].emoji
+        coIconElement.style.color = WTO_Standard[currentGrade].color
+        const coConditionElement = document.querySelector('.co-condition')
+        coConditionElement.textContent = WTO_Standard[currentGrade].condition
+        coConditionElement.style.color = WTO_Standard[currentGrade].color
+        
+        let coPercent = Math.round((coValue/5.5)*100)
+        const coProgress = document.querySelector('.co-progress-done');
+        coProgress.setAttribute('data-done', coPercent)
+        coProgress.style.width = coProgress.getAttribute('data-done') + '%';
+        coProgress.style.background = WTO_Standard[currentGrade].background
+        coProgress.style.opacity = 1;
+
+        currentGrade = getNo2Grade(no2Value)
+        document.querySelector('.no2-value').textContent = no2Value + 'ppm'
+        const no2IconElement = document.getElementById('no2-icon')
+        no2IconElement.className = WTO_Standard[currentGrade].emoji
+        no2IconElement.style.color = WTO_Standard[currentGrade].color
+        const no2ConditionElement = document.querySelector('.no2-condition')
+        no2ConditionElement.textContent = WTO_Standard[currentGrade].condition
+        no2ConditionElement.style.color = WTO_Standard[currentGrade].color
+
+        
+        let no2Percent = Math.round((no2Value/0.06)*100)
+        const no2Progress = document.querySelector('.no2-progress-done');
+        no2Progress.setAttribute('data-done', no2Percent)
+        no2Progress.style.width = no2Progress.getAttribute('data-done') + '%';
+        no2Progress.style.background = WTO_Standard[currentGrade].background
+        no2Progress.style.opacity = 1;
+        
 
     }
 });
